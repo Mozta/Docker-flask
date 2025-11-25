6c2d5610f5c5d0f8658f35ec38179587612ea5fd
mozta <perezaguirrerafael@gmail.com>
2025-11-25 10:09:21 -0600

    postgres

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,5 +1,5 @@
 from flask.cli import FlaskGroup
-from project import app, db
+from project import app, db, User
 
 cli = FlaskGroup(app)
 
@@ -9,6 +9,12 @@ def create_db():
     db.create_all()
     db.session.commit()
     print("Database tables created.")
+
+@cli.command("seed_db")
+def seed_db():
+    db.session.add(User(email="rafa@correo.com"))
+    db.session.commit()
+    print("Database seeded with initial data.")
 
 if __name__ == "__main__":
     cli()
